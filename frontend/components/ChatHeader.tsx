9f66de2a564fff9d17149f0b3eca89ea2914e9a0
Thing <133474899+Sjdjdiejdrirhdkjej@users.noreply.github.com>
2025-06-30 04:52:10 +0000

    feat: Add application logo to header

--- frontend/components/ChatHeader.tsx
+++ frontend/components/ChatHeader.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Image from 'next/image';
 
 const ChatHeader: React.FC = () => {
   return (
     <div className="header">
+      <Image src="/logo.jpeg" alt="Manus Logo" width={50} height={50} />
       <h1>Manus</h1>
     </div>
   );
