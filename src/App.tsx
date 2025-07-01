e77cbe61af6a438a360ef8168d2606ac9ba1d439
Thing <133474899+Sjdjdiejdrirhdkjej@users.noreply.github.com>
2025-07-01 04:13:21 +0000

    Feat: Tried to make it fullscreen

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ function App() {
   };
 
   return (
-    <div className="chat-container">
+    <div className="chat-container" style={{ position: 'fixed', top: '0', right: '0', bottom: '0', left: '0' }, header: "Chat Interface"}>
       <div className="message-list">
         {messages.map((message, index) => (
           <div key={index} className={`message-bubble ${message.sender}-message`}>
